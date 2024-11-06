c7371fe2afe186a0fbfad76bdcd2f92143fec941
ReylanLugo <clockr13@gmail.com>
2024-11-06 15:59:22 -0400

    setup nextjs with multi tenant middleware

---
+++ middleware.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  // Get the hostname from the request headers
+  const hostname = request.headers.get("host") || "";
+  // Split port from hostname
+  const [host] = hostname.split(":");
+  // Split subdomain from hostname
+  const hostParts = host.split(".");
+
+  let subdomain = null;
+  if (hostParts.length > 1) {
+    subdomain = hostParts[0];
+  }
+
+  if (!subdomain || subdomain === "www" || subdomain === "localhost") {
+    return NextResponse.next();
+  }
+
+  // Create a new Headers object with the request headers
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-subdomain", subdomain);
+
+  // Return the modified request headers
+  return NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
+}
+
+export const config = {
+  matcher: "/:path*",
+};
