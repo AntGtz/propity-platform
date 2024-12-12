ebd77790f0a0c4f13f4502c423767decbc8a824f
ReylanLugo <clockr13@gmail.com>
2024-12-12 12:51:35 -0400

    delete prisma

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,59 +1,7 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
-import prisma from "@/lib/prisma/db";
-import Credentials from "next-auth/providers/credentials";
-import bcrypt from "bcryptjs";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    Credentials({
-      name: "Credentials",
-      credentials: {
-        email: {
-          label: "Email",
-          type: "email",
-          placeholder: "john.doe@example.com",
-        },
-        password: {
-          label: "Password",
-          type: "password",
-          placeholder: "Your password",
-        },
-      },
-      async authorize(credentials) {
-        if (
-          !credentials ||
-          typeof credentials.email !== "string" ||
-          typeof credentials.password !== "string"
-        ) {
-          return null;
-        }
-        const user = await prisma.user.findUnique({
-          where: {
-            email: credentials.email as string,
-          },
-        });
-        if (!user) {
-          return null;
-        }
-        if (!user.emailVerified) {
-          return null;
-        }
-        // Verify the password using bcrypt
-        const isValid = await bcrypt.compare(
-          credentials.password,
-          user.password!
-        );
-
-        if (isValid) {
-          return user;
-        } else {
-          return null;
-        }
-      },
-    }),
-  ],
+  providers: [],
   session: {
     strategy: "jwt",
   },
