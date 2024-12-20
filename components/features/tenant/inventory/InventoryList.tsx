eb8a00e03307a493e92e5b5615098240569def32
ReylanLugo <clockr13@gmail.com>
2024-12-20 01:27:24 -0400

    add link to property profile in inventoryList

--- components/features/tenant/inventory/InventoryList.tsx
+++ components/features/tenant/inventory/InventoryList.tsx
@@ -3,6 +3,7 @@
 import { InventoryCard } from "@/components/features/tenant/inventory/InventoryCard";
 import { PropertyData } from "@/type/tenantData";
 import { useInventoryContext } from "@/components/features/tenant/inventory/InventoryContext";
+import Link from "next/link";
 
 export const InventoryList = () => {
   const { propertiesList, entity } = useInventoryContext();
@@ -15,20 +16,24 @@ export const InventoryList = () => {
         }
       >
         {propertiesList.map((property: PropertyData) => (
-          <InventoryCard
+          <Link
             key={property.id}
-            image={property.images[0].thumbnail}
-            address={property.location!.address}
-            price={property.operations[0]!.price}
-            comision={property.operations[0].commission}
-            rooms={property.rooms!}
-            bathrooms={property.bathrooms!}
-            phone={property.owner.phone}
-            facebook={entity?.contact?.facebook}
-            instagram={entity?.contact?.instagram}
-            totalSpace={property.surface.total}
-            totalBuiltSpace={property.surface.built}
-          />
+            href={"/propiedad/" + entity?.id + "/" + property.id}
+          >
+            <InventoryCard
+              image={property.images[0].thumbnail}
+              address={property.location!.address}
+              price={property.operations[0]!.price}
+              comision={property.operations[0].commission}
+              rooms={property.rooms!}
+              bathrooms={property.bathrooms!}
+              phone={property.owner.phone}
+              facebook={entity?.contact?.facebook}
+              instagram={entity?.contact?.instagram}
+              totalSpace={property.surface.total}
+              totalBuiltSpace={property.surface.built}
+            />
+          </Link>
         ))}
       </div>
     </>
