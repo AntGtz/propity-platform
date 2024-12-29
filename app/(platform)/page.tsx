46455ef0120baba23115c985b26bafd3ed0df224
ReylanLugo <clockr13@gmail.com>
2024-12-29 13:52:45 -0400

    add BenefitsSection.tsx and PartnersCarousel.tsx into new landing platform

--- app/(platform)/page.tsx
+++ app/(platform)/page.tsx
@@ -1,17 +1,19 @@
 import Hero from "@/components/features/platform/landing/Hero";
-import Indicators from "@/components/features/platform/landing/Indicators";
 import CTA from "@/components/features/platform/landing/CTA";
 import ExploreTheProduct from "@/components/features/platform/landing/ExploreTheProduct";
 import PricingSection from "@/components/features/platform/landing/PricingSection";
 import Features from "@/components/features/platform/landing/Features";
 import LastCTA from "@/components/features/platform/landing/LastCTA";
 import FAQ from "@/components/features/platform/FAQ";
+import PartnersCarousel from "@/components/features/platform/landing/PartnersCarousel";
+import BenefitsSection from "@/components/features/platform/landing/BenefitsSection";
 
 export default async function Home() {
   return (
     <main>
       <Hero />
-      <Indicators />
+      <PartnersCarousel />
+      <BenefitsSection />
       <CTA />
       <ExploreTheProduct />
       <PricingSection />
