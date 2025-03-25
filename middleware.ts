2238f9d03747a85653e98c5a8bd83eab391d45cc
AntGtz <tranetrose@gmail.com>
2025-03-25 12:20:47 -0600

    change middleware

--- middleware.ts
+++ middleware.ts
@@ -1,46 +1,46 @@
-import { NextRequest, NextResponse } from "next/server";
-import { auth } from "./lib/auth/auth";
+// import { NextRequest, NextResponse } from "next/server";
+// import { auth } from "./lib/auth/auth";
 
-export default auth((request: NextRequest) => {
-  const url = request.nextUrl;
-  const hostname = request.headers.get("host");
+// export default auth((request: NextRequest) => {
+//   const url = request.nextUrl;
+//   const hostname = request.headers.get("host");
 
-  // Define allowed Domains (localhost and production domain)
-  const allowedDomains = [
-    "propity-platform.vercel.app",
-    "propity.mx",
-    "localhost:3000",
-    "propity.io",
-    "propity-git-development-reylans-projects.vercel.app",
-  ];
+//   // Define allowed Domains (localhost and production domain)
+//   const allowedDomains = [
+//     "propity-platform.vercel.app",
+//     "propity.mx",
+//     "localhost:3000",
+//     "propity.io",
+//     "propity-git-development-reylans-projects.vercel.app",
+//   ];
 
-  // Verify if hostname exists in allowed domains
-  const isAllowedDomain =
-    hostname && allowedDomains.some((domain) => hostname.includes(domain));
+//   // Verify if hostname exists in allowed domains
+//   const isAllowedDomain =
+//     hostname && allowedDomains.some((domain) => hostname.includes(domain));
 
-  // Extract the possible subdomain in the URL
-  const subdomain = hostname ? hostname.split(".")[0] : "";
+//   // Extract the possible subdomain in the URL
+//   const subdomain = hostname ? hostname.split(".")[0] : "";
 
-  const isLocalhost = hostname?.includes("localhost");
+//   const isLocalhost = hostname?.includes("localhost");
 
-  if (isAllowedDomain) {
-    const parts = hostname.split(".").length;
+//   if (isAllowedDomain) {
+//     const parts = hostname.split(".").length;
 
-    if ((isLocalhost && parts <= 1) || (!isLocalhost && parts <= 2)) {
-      return NextResponse.next();
-    }
-  }
+//     if ((isLocalhost && parts <= 1) || (!isLocalhost && parts <= 2)) {
+//       return NextResponse.next();
+//     }
+//   }
 
-  if (subdomain) {
-    // Rewrite the URL in the dynamic route based in the subdomain
-    return NextResponse.rewrite(
-      new URL(`/${subdomain}${url.pathname}`, request.url),
-    );
-  }
+//   if (subdomain) {
+//     // Rewrite the URL in the dynamic route based in the subdomain
+//     return NextResponse.rewrite(
+//       new URL(`/${subdomain}${url.pathname}`, request.url),
+//     );
+//   }
 
-  return new Response(null, { status: 404 });
-});
+//   return new Response(null, { status: 404 });
+// });
 
-export const config = {
-  matcher: ["/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)"],
-};
+// export const config = {
+//   matcher: ["/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)"],
+// };
